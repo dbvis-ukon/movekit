31b116115c1fc5a1bf2beb42248faf967015475f
Eren Cakmak <22754816+eren-ck@users.noreply.github.com>
2019-09-11 15:38:39 +0200

    Changed classifiers license text

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ CLASSIFIERS = """\
 Development Status :: 2 - Pre-Alpha
 Intended Audience :: Science/Research
 Intended Audience :: Developers
-License :: OSI Approved :: GPLv3
+License :: OSI Approved :: GNU General Public License v3 (GPLv3)
 Programming Language :: Python
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.5
