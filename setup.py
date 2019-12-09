c82a9507451a11de105b0f27d55a56d3b676c8e3
Jolle Jolles <j.w.jolles@gmail.com>
2019-12-09 17:43:07 +0100

    Improved setup file

--- setup.py
+++ setup.py
@@ -1,65 +1,61 @@
 #!/usr/bin/env python
-"""MOVEKIT
--Simple and effective tools for the analysis of movement data!
-"""
+
 from setuptools import setup, find_namespace_packages
+import sys
 
-CLASSIFIERS = """\
-Development Status :: 2 - Pre-Alpha
-Intended Audience :: Science/Research
-Intended Audience :: Developers
-License :: OSI Approved :: GNU General Public License v3 (GPLv3)
-Programming Language :: Python
-Programming Language :: Python :: 3
-Programming Language :: Python :: 3.5
-Programming Language :: Python :: 3.6
-Programming Language :: Python :: 3.7
-Topic :: Software Development
-Topic :: Scientific/Engineering
-Operating System :: Microsoft :: Windows
-Operating System :: Unix
-Operating System :: MacOS
-"""
+exec(open("movekit/__version__.py").read())
 
-MAJOR = 0
-MINOR = 1
-MICRO = 4
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+DESCRIPTION="""Simple and effective tools for the analysis of movement data"""
+DISTNAME="movekit"
+AUTHOR="Arjun Majumdar, Eren Cakmak, Jolle Jolles"
+AUTHOR_EMAIL="arjun.majumdar@uni-konstanz.de, eren.cakmak@uni-konstanz.de,"\
+              "j.w.jolles@gmail.com"
+MAINTAINER="dbvis-ukon"
+MAINTAINER_EMAIL="eren.cakmak@uni-konstanz.de"
+URL="https://github.com/dbvis-ukon/movekit"
+DOWNLOAD_URL="https://github.com/JolleJolles/pirecorder/archive/v2.0.0.tar.gz"
 
+with open("README.md") as f:
+    readme = f.read()
 
-def setup_package():
+if __name__ == "__main__":
 
-    with open("README.md") as f:
-        long_description = f.read()
+    setup(name=DISTNAME,
+          author=AUTHOR,
+          autor_email=AUTHOR_EMAIL,
+          maintainer=MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          description=DESCRIPTION,
+          long_description=long_description,
+          long_description_content_type="text/markdown",
+          url=URL,
+          install_requires=["tsfresh>=0.12.0",
+                            "pandas>=0.20.3,<=0.23.4",
+                            "scipy>=1.3.1",
+                            "xlrd>=1.2.0",
+                            "seaborn>=0.9.0"],
+        version=__version__,
 
-    metadata = dict(
-        name='movekit',
-        maintainer="dbvis-ukon",
-        maintainer_email="eren.cakmak@uni-konstanz.de",
-        description="Simple and effective tools for the analysis of movement data",
-        author="Arjun Majumdar, Eren Cakmak, Jolle Jolles",
-        author_email="arjun.majumdar@uni-konstanz.de, eren.cakmak@uni-konstanz.de, jjolles@orn.mpg.de",
-        version=VERSION,
-        long_description=long_description,
-        long_description_content_type="text/markdown",
         url="https://github.com/dbvis-ukon/movekit",
-        license='License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+        license="License :: OSI Approved :: GNU General Public License v3",
         platforms=["Windows", "Linux", "Mac OS-X"],
         packages=find_namespace_packages(
             include=['movekit.*'],
             exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-        install_requires=['tsfresh>=0.12.0',
-                          'pandas>=0.20.3,<=0.23.4',
-                          'scipy>=1.3.1',
-                          'xlrd>=1.2.0',
-                          'seaborn>=0.9.0',
-                          ],
         python_requires='>=3.5',
+        classifiers=[
+                    "Development Status :: 2 - Pre-Alpha",
+                    "Intended Audience :: Science/Research",
+                    "Intended Audience :: Developers",
+                    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                    "Programming Language :: Python",
+                    "Programming Language :: Python :: 3",
+                    "Programming Language :: Python :: 3.5",
+                    "Programming Language :: Python :: 3.6",
+                    "Programming Language :: Python :: 3.7",
+                    "Topic :: Software Development",
+                    "Topic :: Scientific/Engineering",
+                    "Operating System :: Microsoft :: Windows",
+                    "Operating System :: Unix",
+                    "Operating System :: MacOS"],
     )
-
-    setup(**metadata)
-
-
-if __name__ == '__main__':
-    setup_package()
