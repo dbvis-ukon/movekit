3f4c1f224a3901a7826e6ccc70554e61ba4368e8
CWebkas <lukeweixler@googlemail.com>
2020-11-20 12:16:17 +0100

    setup

---
+++ env/lib/python3.8/site-packages/piptools/_compat/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# flake8: noqa
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import six
+
+from .pip_compat import BAR_TYPES, PIP_VERSION, parse_requirements
+
+if six.PY2:
+    from .tempfile import TemporaryDirectory
+else:
+    from tempfile import TemporaryDirectory
