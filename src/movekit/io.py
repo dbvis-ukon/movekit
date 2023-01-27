565e2d14b5f471ef6b135a59f2ed3d7f65c2ebd6
Tim Kleinlein <tim.kleinlein@uni-konstanz.de>
2023-01-27 11:05:21 +0100

    - adjusted existing import function to also check third dimension (if available) for right format - deleted read_gejson and replaced it with function read_with_geometry which is more universal in a way that it allows user to import several files (geojson, geopackage, shapefile, ...), store data as geopandas, and checks required columns for correct format.

--- src/movekit/io.py
+++ src/movekit/io.py
@@ -5,12 +5,14 @@ from pandas.errors import EmptyDataError
 import warnings
 from .preprocess import convert_latlon
 import geopandas as gpd
+import shapely
 
-def parse_csv(path_to_file, time_format):
+def parse_csv(path_to_file, time_format, three_dim):
     """
     Read CSV file into Pandas DataFrame.
     :param path_to_file: Complete path/relative path to CSV file along with file name.
     :param time_format: If time is given in an unusual format, the format has to be indicated for the conversion.
+    :param three_dim: Boolean defining whether data contains three dimensional coordinates
     :return: Pandas DataFrame containing imported data.
     """
     try:
@@ -23,10 +25,23 @@ def parse_csv(path_to_file, time_format):
         # change column names all to lower case values
         data.columns = map(str.lower, data.columns)
 
-        format_col = ['time', 'animal_id', 'x', 'y']
+        if three_dim:
+            format_col = ['time', 'animal_id', 'x', 'y', 'z']
+        else:
+            format_col = ['time', 'animal_id', 'x', 'y']
 
         # check if all required columns are there in the right format
-        if 'time' in data and 'animal_id' in data and 'x' in data and 'y' in data:
+        if three_dim:
+            if 'time' in data and 'animal_id' in data and 'x' in data and 'y' in data and 'z' in data:
+                contains_columns = True
+            else:
+                contains_columns = False
+        else:
+            if 'time' in data and 'animal_id' in data and 'x' in data and 'y' in data:
+                contains_columns = True
+            else:
+                contains_columns = False
+        if contains_columns:
             # Check if 'time' attribute is integer-
             if is_numeric_dtype(data['time']):
                 data.sort_values(['time', 'animal_id'],
@@ -43,7 +58,7 @@ def parse_csv(path_to_file, time_format):
                                  ascending=True,
                                  inplace=True)
                 data.drop(data.filter(regex="unname"), axis=1, inplace=True)
-            # check if 'x' and 'y' are numeric-
+            # check if 'x' and 'y' and 'z' are numeric-
             if not is_numeric_dtype(data['x']):
                 warnings.warn(
                     "'x' attribute has to be of numeric data type for most of the analyses to be performed"
@@ -52,6 +67,11 @@ def parse_csv(path_to_file, time_format):
                 warnings.warn(
                     "'y' attribute has to be of numeric data type for most of the analyses to be performed"
                 )
+            if three_dim:
+                if not is_numeric_dtype(data['z']):
+                    warnings.warn(
+                        "'z' attribute has to be of numeric data type for most of the analyses to be performed"
+                    )
 
             # Check if 'heading_angle' attribute is given in CSV file-
             if 'heading_angle' in data and np.issubdtype(
@@ -80,12 +100,13 @@ def parse_csv(path_to_file, time_format):
 
 
 
-def parse_excel(path_to_file, sheet, time_format):
+def parse_excel(path_to_file, sheet, time_format, three_dim):
     """
     Read Excel file into Pandas DataFrame
     :param path_to_file: Complete path/relative path to Excel file along with file name
     :param sheet: name of specific sheet given, by default first sheet of the excel workbook
     :param time_format: If time is given in an unusual format, the format has to be indicated for the conversion.
+    :param three_dim: Boolean defining whether data contains three dimensional coordinates
     :return: Pandas DataFrame containing imported data.
     """
     try:
@@ -101,10 +122,23 @@ def parse_excel(path_to_file, sheet, time_format):
         # change column names all to lower case values
         data.columns = map(str.lower, data.columns)
 
-        format_col = ['time', 'animal_id', 'x', 'y']
+        if three_dim:
+            format_col = ['time', 'animal_id', 'x', 'y', 'z']
+        else:
+            format_col = ['time', 'animal_id', 'x', 'y']
 
         # check if all required columns are there in the right format
-        if 'time' in data and 'animal_id' in data and 'x' in data and 'y' in data:
+        if three_dim:
+            if 'time' in data and 'animal_id' in data and 'x' in data and 'y' in data and 'z' in data:
+                contains_columns = True
+            else:
+                contains_columns = False
+        else:
+            if 'time' in data and 'animal_id' in data and 'x' in data and 'y' in data:
+                contains_columns = True
+            else:
+                contains_columns = False
+        if contains_columns:
             # Check if 'time' attribute is integer-
             if is_numeric_dtype(data['time']):
                 data.sort_values(['time', 'animal_id'],
@@ -120,7 +154,7 @@ def parse_excel(path_to_file, sheet, time_format):
                 data.sort_values(['time', 'animal_id'],
                                  ascending=True,
                                  inplace=True)
-            # check if 'x' and 'y' are numeric-
+            # check if 'x' and 'y' and 'z' are numeric-
             if not is_numeric_dtype(data['x']):
                 warnings.warn(
                     "'x' attribute has to be of numeric data type for most of the analyses to be performed"
@@ -129,6 +163,12 @@ def parse_excel(path_to_file, sheet, time_format):
                 warnings.warn(
                     "'y' attribute has to be of numeric data type for most of the analyses to be performed"
                 )
+
+            if three_dim:
+                if not is_numeric_dtype(data['z']):
+                    warnings.warn(
+                        "'z' attribute has to be of numeric data type for most of the analyses to be performed"
+                    )
 
             # Check if 'heading_angle' attribute is given in CSV file-
             if 'heading_angle' in data and np.issubdtype(
@@ -156,12 +196,13 @@ def parse_excel(path_to_file, sheet, time_format):
             path_to_file))
 
 
-def read_data(path, sheet=0, time_format="undefined"):
+def read_data(path, sheet=0, time_format="undefined", three_dim=False):
     """
     Function to import data from 'csv', 'xlsx' and 'xls' files.
     :param path: Complete path/relative path to Excel file along with file name
     :param sheet: name of specific sheet given, by default first sheet of the excel workbook
     :param time_format: If time is given in an unusual format, the format has to be indicated for the conversion.
+    :param three_dim: Boolean defining whether data contains three dimensional coordinates
     :return: Pandas DataFrame containing imported data.
     """
     # Call appropriate IO function based on file extension
@@ -170,11 +211,11 @@ def read_data(path, sheet=0, time_format="undefined"):
 
     if file_split[-1] == 'csv':
 
-        return parse_csv(path, time_format)
+        return parse_csv(path, time_format, three_dim)
     elif file_split[-1] == 'xlsx':
-        return parse_excel(path, sheet, time_format)
+        return parse_excel(path, sheet, time_format, three_dim)
     elif file_split[-1] == 'xls':
-        return parse_excel(path, sheet, time_format)
+        return parse_excel(path, sheet, time_format, three_dim)
     else:
         raise ValueError(f'File extension {file_split[-1]} can not be imported. Imported file has to be of type ".csv" or ".xlsx" or ".xls".')
 
@@ -219,47 +260,149 @@ def read_movebank(path_to_file, animal_id = 'individual-local-identifier'):
     return data
 
 
-def read_geojson(path, animal_id="name", time="time"):
+def read_with_geometry(path, animal_id="name", time="time", x="x", y="y",  z="z", coordinate_conversion=False, time_format="undefined", geopandas = True):
     """
-    Function to import geojson files.
+    Function to import files containing both data and geometry (f.e. GeoPackage, (Geo)json, Shapefile and many more).
     :param path: Complete path/relative path to file along with file name
-    :param animal_id: Key name of the unique animal identifier (as defined as property value in the geojson object)
-    :param time: Key name of time (as defined as property value in the geojson object)
-    return: Data frame in a format required for using the movekit package.
+    :param animal_id: Key name of the unique animal identifier (as f.e. defined as property value in the geojson feature)
+    :param time: Key name of time (as defined f.e. as property value in the geojson feature)
+    :param x: Key name of x variable (as defined f.e. as property value in the geojson feature)
+    :param y: Key name of y variable (as defined f.e. as property value in the geojson feature)
+    :param z: Key name of z variable (as defined f.e. as property value in the geojson feature)
+    :param coordinate_conversion: Boolean defining whether the x,y (and z) coordinates are stored in geometry object of imported data (f.e. as geometry value in geojson feature).
+    Note that if coordinate_conversion=True function searches for point object in geometry column and converts coordinates to individual columns of returned data frame.
+    (In case of multiple geometries for an observation, so called 'geometry collections', the first point object is converted.)
+    :param time_format: If time is given in an unusual format, the format has to be indicated for the conversion.
+    :param geopandas: Boolean defining whether the returned data frame is a geopandas data frame (containing geometry objects in column 'geometry') or a pandas data frame (not containing a 'geometry' column).
+    return: Geopandas or pandas data frame in a format required for using the movekit package.
     """
 
     # check extension
     file_split = path.split(".")
-    if file_split[-1] != 'json' and file_split[-1] != 'geojson':
-        raise ValueError(
-            f'File extension {file_split[-1]} can not be imported with this function. Imported file has to be of type ".json" or ".geojson".')
+    if file_split[-1] != 'json' and file_split[-1] != 'geojson' and file_split[-1] != 'gpkg' and file_split[-1] != 'shp':
+        warnings.warn(
+            f'This function might not be able to import {file_split[-1]} files. If the import id unsuccessful try file of type ".json", ".geojson", ".gpkg" or ".shp".')
 
     # extract and convert data
     data = gpd.read_file(path)
-    # get coordinates from points
-    x = []
-    for i in range(len(data)):
-        x.append(data['geometry'][i].x)
-    y = []
-    for i in range(len(data)):
-        y.append(data['geometry'][i].y)
-    if len(list(data["geometry"][0].coords)[0]) == 3:  # check for 3d
-        z = []
-        for i in range(len(data)):
-            z.append(data['geometry'][i].z)
 
-        df = pd.DataFrame({'time': data[time],
-                           'animal_id': data[animal_id],
-                           'x': x,
-                           'y': y,
-                           'z': z})
+    if coordinate_conversion:
+
+        # check first observation to see if type in geometry column is point
+        if isinstance(data['geometry'][0], shapely.geometry.point.Point):
+            # get coordinates from points
+            x = []
+            y = []
+            if len(list(data["geometry"][0].coords)[0]) == 3:  # check for 3d
+                contains_z = True
+                z = []
+                for i in range(len(data)):
+                    x.append(data['geometry'][i].x)
+                    y.append(data['geometry'][i].y)
+                    z.append(data['geometry'][i].z)
+            else:
+                contains_z = False
+                for i in range(len(data)):
+                    x.append(data['geometry'][i].x)
+                    y.append(data['geometry'][i].y)
+
+        elif isinstance(data['geometry'][0], shapely.geometry.collection.GeometryCollection):
+            for j in range(len(data['geometry'][0].geoms)): #if it is geometry collection, find position of the point
+                if isinstance(data['geometry'][0].geoms[j], shapely.geometry.point.Point):
+                    break
+            if not isinstance(data['geometry'][0].geoms[j], shapely.geometry.point.Point):  # if no object in geometry collection is point
+                warnings.warn('The geometry column of the given data does not contain any point object from which the coordinates could be extracted.'
+                              'Thus, if data already contains columns except the geometry column with coordinates redefine coordinate_conversion to False,'
+                              'otherwise the data can not be imported with this function.')
+                return
+            # get coordinates from points
+            x = []
+            y = []
+            if len(list(data["geometry"][0].geoms[j].coords)[0]) == 3:  # check for 3d
+                contains_z = True
+                z = []
+                for i in range(len(data)):
+                    x.append(data['geometry'][i].geoms[j].x)
+                    y.append(data['geometry'][i].geoms[j].y)
+                    z.append(data['geometry'][i].geoms[j].z)
+            else:
+                contains_z = False
+                for i in range(len(data)):
+                    x.append(data['geometry'][i].geoms[j].x)
+                    y.append(data['geometry'][i].geoms[j].y)
+        else:  # geometry column has no point object
+            warnings.warn(
+                'The geometry column of the given data does not contain any point object from which the coordinates could be extracted.'
+                'Thus, if data already contains columns except the geometry column with coordinates redefine coordinate_conversion to False,'
+                'otherwise the data can not be imported with this function.')
+            return
+        data['x'] = x
+        data['y'] = y
+        if contains_z:
+            data['z'] = z
+
+        # rename columns
+        data.rename(columns={f'{animal_id}': 'animal_id', f'{time}': 'time'}, inplace=True)
+
     else:
-        df = pd.DataFrame({'time': data[time],
-                           'animal_id': data[animal_id],
-                           'x': x,
-                           'y': y})
+        if z in data.columns:
+            data.rename(columns={f'{animal_id}': 'animal_id', f'{time}': 'time', f'{x}': 'x', f'{y}': 'y', f'{z}': 'z'},
+                        inplace=True)
+        else:
+            data.rename(columns={f'{animal_id}': 'animal_id', f'{time}': 'time', f'{x}': 'x', f'{y}': 'y'},
+                        inplace=True)
 
-    df.sort_values(['time', 'animal_id'],
-                     ascending=True,
-                     inplace=True)
-    return df.reset_index(drop=True)
+
+    # check if all required columns are there in the right format
+    # Check if 'time' attribute is integer-
+    if is_numeric_dtype(data['time']):
+        data.sort_values(['time', 'animal_id'],
+                         ascending=True,
+                         inplace=True)
+        # Check if 'time' attribute is string-
+    elif is_string_dtype(data['time']):
+        if time_format == "undefined":
+            data['time'] = pd.to_datetime(data['time'])
+            warnings.warn(
+                'As time was converted and no format was given as parameter, please check if the time conversion is correct.')
+        else:
+            data['time'] = pd.to_datetime(data['time'], format=time_format)
+        data.sort_values(['time', 'animal_id'],
+                         ascending=True,
+                         inplace=True)
+    # check if 'x' and 'y' and 'z' are numeric-
+    if not is_numeric_dtype(data['x']):
+        warnings.warn(
+            "'x' attribute has to be of numeric data type for most of the analyses to be performed"
+        )
+    if not is_numeric_dtype(data['y']):
+        warnings.warn(
+            "'y' attribute has to be of numeric data type for most of the analyses to be performed"
+        )
+    if 'z' in data.columns:
+        if not is_numeric_dtype(data['z']):
+            warnings.warn(
+                "'z' attribute has to be of numeric data type for most of the analyses to be performed"
+            )
+
+    # Check if 'heading_angle' attribute is given in CSV file-
+    if 'heading_angle' in data and np.issubdtype(
+            data['heading_angle'].dtype, np.number):
+        print(
+            "\n'heading_angle' attribute is found and will be processed\n"
+        )
+
+    # Check if time contains duplicates and if so execute warning
+    grouped_df = data.groupby("animal_id")
+    for aid in grouped_df.groups.keys():
+        if (grouped_df.get_group(aid)["time"].duplicated().any()):
+            warnings.warn(
+                "For some animals there are duplicate values for time! This might indicate some error in the "
+                "data and can lead to incorrect analysis."
+            )
+            break
+
+    if not geopandas:  # if returned data frame is desired to be pandas df and not geopandas df
+        data = pd.DataFrame(data.drop(columns=['geometry']))
+
+    return data
