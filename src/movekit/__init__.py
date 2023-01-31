3d440807000f25a8b4d74e9f0ffd99807b208029
Tim Kleinlein <tim.kleinlein@uni-konstanz.de>
2023-01-31 09:02:57 +0100

    added plot_heatmap()

--- src/movekit/__init__.py
+++ src/movekit/__init__.py
@@ -27,6 +27,6 @@ from .feature_extraction import ts_feature, ts_all_features, explore_features_ge
     outlier_detection, group_movement, explore_features, compute_direction_angle, compute_turning_angle, split_movement_trajectory, movement_stopping_durations,hausdorff_distance, getis_ord
 from .clustering import dtw_matrix, get_heading_difference, compute_polarization, get_spatial_objects, compute_centroid_direction, clustering, clustering_with_splits
 # noqa: E402
-from .plot import plot_animal, plot_pace, plot_movement, animate_movement, plot_geodata, save_geodata_map, save_animation_plot, plot_animal_timesteps  # noqa: E402
+from .plot import plot_animal, plot_pace, plot_movement, animate_movement, plot_geodata, save_geodata_map, save_animation_plot, plot_animal_timesteps, plot_heatmap # noqa: E402
 
 from .network import network_time_graphlist
